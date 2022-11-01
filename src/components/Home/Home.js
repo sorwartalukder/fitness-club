f727a2e5f305cacd8a508188fe921fdda182d194
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-01 12:18:19 +0600

    add website  layout

---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <div>
+            <Row>
+
+                <Col lg='8'>
+                    card
+                </Col>
+
+                <Col lg='4'>
+                    activity
+                </Col>
+
+            </Row>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/doc.js
@@ -0,0 +1,7 @@
+// repo link :   https://github.com/Programming-Hero-Web-Course4/b6-ultra-active-club-sorwartalukder
+
+// installation
+
+//bootstrap
+// react bootstrap
+// react icons 
