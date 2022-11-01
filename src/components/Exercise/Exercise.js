c35ecdaca7bf43380406c90cc5787722c78583dc
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-01 14:16:27 +0600

    add exercise list

---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { FaPlus } from 'react-icons/fa';
+
+const Exercise = ({ exercise }) => {
+    const { age, body, picture, time, title } = exercise
+    return (
+        <div className='col'>
+            <Card className='bg-white'>
+                <Card.Img variant="top" src={picture} style={{ height: '200px' }} />
+                <Card.Body>
+                    <Card.Title className='fw-bold text-color'>{title}</Card.Title>
+                    <Card.Text>
+                        {body.slice(0, 100)}
+                    </Card.Text>
+                    <Card.Text className='m-1 fw-semibold text-color'>
+                        For Age :  {age}
+                    </Card.Text>
+                    <Card.Text className='fw-semibold text-color' >
+                        Time required :  {time}
+                    </Card.Text>
+                    <div className='text-center '>
+                        <button className='btn btn-info fw-semibold text-color'>ADD <FaPlus></FaPlus></button>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default Exercise;
---
+++ src/components/ExerciseList/ExerciseList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Exercise from '../Exercise/Exercise';
+import { FaDumbbell } from "react-icons/fa";
+
+const ExerciseList = () => {
+    const [exerciseList, setExerciseList] = useState([])
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setExerciseList(data))
+    }, [])
+    return (
+        <div>
+            <h3 className=' text-color py-3'>Select today’s exercise <FaDumbbell className='ms-2 fs-1'></FaDumbbell></h3>
+            <div className='row row-cols-1 row-cols-md-2 px-lg-5 g-4'>
+                {
+                    exerciseList.map(exercise => <Exercise
+                        key={exercise.id}
+                        exercise={exercise}
+                    ></Exercise>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ExerciseList;
